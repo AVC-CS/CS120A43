90f7c199be4fcac9ab54914c171be32755ddd8a6
Kyu Lee <kwooler@gmail.com>
2023-09-02 15:04:50 -0700

    A43

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 // Make a program that finds the minimum and maximum values among three integer values.
 
-// int num1, num2, num3;
-// cin >> num1 >> num2 >> num3;
-// Find the min 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    int min;
+    cin >> num1 >> num2 >> num3;
 
+    /*
+    Code your program here
+    */
 
+    // Use the following statements to print output
+    cout << "The smallest number is  " << min << endl;
+}
